b91a575116eafc999c0fc00cfad85a4edc4ae25a
Dirceu Henrique <dhnasci@hotmail.com>
2021-04-02 19:58:55 -0400

    Add .env handling

--- src/services/EmpresasService.js
+++ src/services/EmpresasService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: process.env.VUE_APP_BASE_API_URL,
     headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json',
